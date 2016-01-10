d40dd8956ab7c82b463e1dc7e2cc91177faea08a
Sprackle <jamiemills505@gmail.com>
2016-01-10 21:40:09 +0000

    Additions To Diary and comments

--- CAT 2015/CAT 2015/CAT 2015/Pages/MasterPage.Master.cs
+++ CAT 2015/CAT 2015/CAT 2015/Pages/MasterPage.Master.cs
@@ -9,14 +9,17 @@ namespace CAT_2015.Pages
 {
     public partial class MasterPage : System.Web.UI.MasterPage
     {
-        CAT_2015.User currentUser = null;
+        CAT_2015.User currentUser = null; // The current session's associated user
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Get the current session's user and set currentUser to it
             currentUser = AppCode.Database.GetLoggedInUser(Session.SessionID);
-            if (currentUser != null)
+            if (currentUser != null) // If a user is found...
             {
+                // Show the user in the corner of the page
                 labelUser.Text = "User: " + currentUser.Username;
             }
+            // If no user is found, the label will display nothing
         }
     }
 }
--- CAT 2015/CAT 2015/CAT 2015/Pages/User/Overview.aspx.cs
+++ CAT 2015/CAT 2015/CAT 2015/Pages/User/Overview.aspx.cs
@@ -9,58 +9,74 @@ namespace CAT_2015.Pages.User
 {
     public partial class Overview : System.Web.UI.Page
     {
-        CAT_2015.User currentUser = null;
+        CAT_2015.User currentUser = null; // The current session's associated user
+        // The achievements that the user has achieved
         List<Achievement> userAchievements = new List<Achievement>();
+        // The achievements that the user has achieved and need to be awarded
         List<Achievement> achievementsToBeAwarded = new List<Achievement>();
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Get the current session's user and set currentUser to it
             currentUser = AppCode.Database.GetLoggedInUser(Session.SessionID);
-            if (currentUser != null)
+            if (currentUser != null) // If a user was found...
             {
+                // Get the achievements that need to be awarded
                 achievementsToBeAwarded = AppCode.Database.GetUserAchievements(currentUser, false, false);
 
-                if (achievementsToBeAwarded.Count > 0)
+                if (achievementsToBeAwarded.Count > 0) // If any were found...
                 {
-                    foreach (Achievement a in achievementsToBeAwarded)
+                    foreach (Achievement a in achievementsToBeAwarded) // Award them
                     {
                         // TODO: Show message boxes for the achievements
                         // TODO: Include awards
+                        // TODO: Add the achievements here to user achievements
                     }
                 }
                 else
                 {
+                    // Otherwise, print to the console that no achievements
+                    // were found for this user to be awarded
                     Console.WriteLine("No achievements to be awarded for user " + currentUser.ID);
                 }
 
+                // Now get the achievements that the user has already had awarded.
                 userAchievements = AppCode.Database.GetUserAchievements(currentUser, false, true);
-                if (userAchievements.Count > 0)
+                if (userAchievements.Count > 0) // If there are any...
                 {
-                    foreach (Achievement a in userAchievements)
+                    foreach (Achievement a in userAchievements) // Take each one and...
                     {
-                        TableRow row = new TableRow();
+                        TableRow row = new TableRow(); // Create a row
                         row.Height = 100;
 
-                        TableCell cell = new TableCell();
+                        TableCell cell = new TableCell(); // Create a cell
 
+                        // Create an achievement element
                         CAT_2015.Pages.User.Controls.AchievementElement achievementElement =
                         (CAT_2015.Pages.User.Controls.AchievementElement)Page.LoadControl
                         ("~/Pages/User/Controls/AchievementElement.ascx");
 
+                        // Put the achievement element in the cell, the cell in the
+                        // row and the row in the achievement table
                         cell.Controls.Add(achievementElement);
                         row.Controls.Add(cell);
                         tableAchievements.Controls.Add(row);
+
+                        // Then set the achievement element to display the current achievement
                         achievementElement.SetAchievement(a, true, currentUser);
                     }
 
+                    // Finally, get the most recent achievements' IDs
                     List<int> recentAchievements = AppCode.Database.GetRecentAchievementIDsForUser(currentUser);
-                    if (recentAchievements.Count > 0)
+                    if (recentAchievements.Count > 0) // If any are found...
                     {
+                        // Match them up to the achievements in userAchievements
                         foreach (Achievement a in userAchievements)
                         {
                             foreach (int id in recentAchievements)
                             {
                                 if (a.ID == id)
                                 {
+                                    // And create a row, cell and achievement element as before
                                     TableRow row = new TableRow();
                                     row.Height = 100;
 
@@ -70,6 +86,7 @@ namespace CAT_2015.Pages.User
                                     (CAT_2015.Pages.User.Controls.AchievementElement)Page.LoadControl
                                     ("~/Pages/User/Controls/AchievementElement.ascx");
 
+                                    // Put them in eachother as before, and set the achievement
                                     cell.Controls.Add(achievementElement);
                                     row.Controls.Add(cell);
                                     tableRecentAchievements.Controls.Add(row);
@@ -82,6 +99,8 @@ namespace CAT_2015.Pages.User
             }
             else
             {
+                // If no user is found, redirect the client to the default home address as defined
+                // by the app settings.
                 Response.Redirect(System.Configuration.ConfigurationManager.AppSettings["DefaultHome"]);
             }
         }
